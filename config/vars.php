bcf828fecd4c2d8aa8ca85ad201911a9feb6ec29
IvanSimic95 <email@isimic.com>
2023-08-20 22:56:29 +0200

    Update vars.php

--- config/vars.php
+++ config/vars.php
@@ -1,5 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'/assets/templates/session.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 date_default_timezone_set('Europe/Bucharest');
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
