9129ec16faaf976779d54e3fd054f1d5ccfd1536
s0mekind <gabeobm@gmail.com>
2023-08-21 21:23:12 +0300

    das

--- assets/templates/bestseller.php
+++ assets/templates/bestseller.php
@@ -2,29 +2,29 @@
          <div class="col-lg-12 col-xl-12">
             <div style="margin-top: 15px;margin-bottom:15px;" class="row">
                 <div class="col-lg-12">
-                    <h2 class="h2-heading">BEST SELLERS</h2>
+                    <h2 class="h2-heading">BEST SELLING PRODUCTS</h2>
                 </div>
                 <div class="col-md-4 col-6 m-auto">
                        <div class="card-body" style="padding:0.8rem">
-                            <h4 class="card-title-prod">PSYCHIC DRAWINGS</h4>
+                            <h4 class="card-title-prod">SOULMATE PSYCHIC DRAWING</h4>
                        </div>
 					    <div style="background-color: #fff;margin-bottom: 20px;" class="card-image">
                             <img class="img-fluid" src="/images/soulmate-03.png" alt="alternative">
 							<div style="padding:5px;margin-bottom:0px;">
 							<p style="font-size: 14px;line-height: 18px;margin-top:5px;margin-bottom: 10px;">Soulmate Healer's psychic drawings capture the essence of your destined Soulmate. Using intuitive abilities, each portrait reveals the face and energy of your other half, awaiting your discovery.</p>
-							<button onclick="location.href='cat01.html'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
+							<button onclick="location.href='soulmate-drawing.php'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
 							</div>
 						</div>
                 </div>
 				<div class="col-md-4 col-6 m-auto">
                        <div class="card-body" style="padding:0.8rem">
-                            <h4 class="card-title-prod">PSYCHIC READINGS</h4>
+                            <h4 class="card-title-prod">FUTURE SPOUSE PSYCHIC DRAWING</h4>
                        </div>
 					    <div style="background-color: #fff;margin-bottom: 20px;" class="card-image">
                             <img class="img-fluid" src="/images/soulmate-03.png" alt="alternative">
 							<div style="padding:5px;margin-bottom:0px;">
 							<p style="font-size: 14px;line-height: 18px;margin-top:5px;margin-bottom: 10px;">Soulmate Healer's psychic drawings capture the essence of your destined Soulmate. Using intuitive abilities, each portrait reveals the face and energy of your other half, awaiting your discovery.</p>
-							<button onclick="location.href='cat01.html'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
+							<button onclick="location.href='future-spouse.php'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
 							</div>
 						</div>
                 </div>
@@ -36,7 +36,7 @@
                             <img class="img-fluid" src="/images/soulmate-03.png" alt="alternative">
 							<div style="padding:5px;margin-bottom:0px;">
 							<p style="font-size: 14px;line-height: 18px;margin-top:5px;margin-bottom: 10px;">Soulmate Healer's psychic drawings capture the essence of your destined Soulmate. Using intuitive abilities, each portrait reveals the face and energy of your other half, awaiting your discovery.</p>
-							<button onclick="location.href='cat01.html'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
+							<button onclick="location.href='purification.php'" style="border-radius: 0.25rem;height: 2rem;" class="form-control-submit-button">READ MORE</button>
 							</div>
 						</div>
                 </div>
