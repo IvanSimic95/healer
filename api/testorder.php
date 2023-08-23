55b78a561417901c4336d91c2061fa0cfe8c8fba
IvanSimic95 <email@isimic.com>
2023-08-23 23:03:04 +0200

    a

--- api/testorder.php
+++ api/testorder.php
@@ -1,4 +1,21 @@
 <?php
-$print = "";
+$print = "Start of print: \n
+";
+foreach($_POST as $key => $value)
+{
+   $print .= 'Key = ' . $key . '<br />';
+   $print .= 'Value= ' . $value;
+   $print .= '\n\n';
+}
 
+// ðŸ‘‡ open a stream
+$file = fopen("text.txt", "w");
+
+// ðŸ‘‡ write to the stream
+fwrite($file, $print);
+
+// ðŸ‘‡ close the stream
+fclose($file);
+
+echo $print;
 ?>
