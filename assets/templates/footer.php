41ccc898dcb2b84a7fb261c867a8f7c31fc301d3
IvanSimic95 <email@isimic.com>
2023-08-26 04:41:05 +0200

    Update footer.php

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -97,6 +97,6 @@ text-align: left;margin-top: 15px;
     <script src="/js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
     <script src="/js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
     <script src="/js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
-    <script src="/js/scripts.js?v=3"></script> <!-- Custom scripts -->
+    <script src="/js/scripts.js?v=4"></script> <!-- Custom scripts -->
 </body>
 </html>						
