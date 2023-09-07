538f6d75cd4c0696a9159b1365f1f28153f8e358
IvanSimic95 <email@isimic.com>
2023-09-07 20:32:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/assets/templates/header.php'; ?>
     <header class="ex-6-header">
         <div class="header-content">
             <div class="container">
-                <div class="row header-box">
+                <div class="header-box" style="background-color:#f5f5f5;">
                 
                    <div class="col-sm-12 col-12">
 					<div style="margin-top:0px;">
-                    <h1>Welcome to Your Safety Place</h1>
-                                <img class="img-fluid" src="/images/12.png" style="float:right;padding:8px;" alt="alternative">
+                    <h2 style="padding-top:10px;">Welcome to Your Safety Place</h2>
+                                <img class="img-fluid" src="/images/12.png" style="padding:8px;width:60%;border-radius:0.75rem;" alt="alternative">
                       
-						<p style="font-size: 14px;line-height: 17px; margin:15px;max-width:none;">
+						<p style="font-size: 16px;line-height: 17px; margin:15px;max-width:none;">
 		In the vast realm of the unseen and the uncharted, where energies weave tales of destinies and souls dance on the rhythms of the universe, there emerges a unique force - The Soulmate Healer. <br> With an ethereal touch and an enigmatic presence, this extraordinary psychic doesn't merely glimpse into the future or the past; they venture deeper, into the intricate tapestries of soul connections. They have a rare gift, an uncanny ability to mend the cosmic ties that bind two souls, healing rifts that span lifetimes and bridging gaps that seem insurmountable.
 		
 		</p> 
@@ -36,19 +36,19 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/assets/templates/header.php'; ?>
 	<div style="background-color:#f5f5f5;margin-right: 0px;margin-left: 0px;border-radius:0.5rem;" class="row">
          <div class="col-lg-12 col-xl-12">
             <div style="margin-top: 15px;margin-bottom:15px;" class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="text-container">
                         <h2>About The Soulmate Healer</h2>
                         <p>Born with an innate gift, the Soulmate Healer doesn't merely perceive the future; he pierces through the veils of time, gazing into the very essence of soulmates. His psychic drawings are more than mere artworks; they are windows into the spiritual realm, capturing the countenance of destined partners and unveiling the love stories written in the stars.</p>
                         <p>A beacon of light in a sea of cosmic connections, the Soulmate Healer possesses a unique ability to tap into the spiritual undercurrents of the universe. With a finely-tuned intuition and a heart attuned to the rhythms of love, they expertly navigate the intricate pathways of soul connections, bringing forth profound insights and revelations.</p>
                         <p>To encounter the Soulmate Healer is to step into a world where love transcends boundaries, where souls find their eternal matches, and where every heart discovers its true calling. In his presence, you'll uncover the depth of your connections and the boundless potential of love that resides within you.</p>
-  <div class="col-lg-6">
+  <div class="col-lg-12">
                     <div class="image-container">
                         <img class="img-fluid" src="images/14.png" alt="alternative">
                     </div> 
                 </div> 
 				<br>
-                        <a style="width:100%;text-align:center;" class="btn-solid-reg" href="/soulmate-drawing.php">ORDER NOW</a>
+                        <a style="width:100%;text-align:center;" class="btn-solid-reg" href="/soulmate-drawing">ORDER NOW</a>
                     </div> <br clear="all">
                 </div>  
    
