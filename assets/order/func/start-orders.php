761a2088f56cd5f63f2ba9346fd498e96b189a79
IvanSimic95 <email@isimic.com>
2023-09-28 22:53:15 +0200

    Update start-orders.php

--- assets/order/func/start-orders.php
+++ assets/order/func/start-orders.php
@@ -313,7 +313,7 @@ if($userSex == "male"){
 
 if($push == 1){
 	$pushTitle = "Order: ".$order_product_nice." - ".$orderPriority."h";
-	$pushMessage = "Price: ".$orderPrice;
+	$pushMessage = "Price: $".$orderPrice;
 curl_setopt_array($ch = curl_init(), array(
 	CURLOPT_URL => "https://api.pushover.net/1/messages.json",
 	CURLOPT_POSTFIELDS => array(
