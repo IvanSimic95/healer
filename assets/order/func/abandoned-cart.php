371fef824e6d7d800611762359ff3878ad03618d
IvanSimic95 <email@isimic.com>
2023-09-29 19:31:41 +0200

    Update abandoned-cart.php

--- assets/order/func/abandoned-cart.php
+++ assets/order/func/abandoned-cart.php
@@ -32,15 +32,15 @@ while($row = $resultpending->fetch_assoc()) {
 			$difference = $delta / 60;
 			echo "Minutes difference: ".$difference;
 
-		
-		/*
+		//REMOVE AFTER TEST 
+		if($user == "10018"){
 			//First create TalkJS User
 			$ch = curl_init();
 			$data = [
 			"id" => $user,
 			"name" => $name,
 			"email" => [$email],
-			"role" => "customer",
+			"role" => "abcustomer",
 			"photoUrl" => "https://avatars.dicebear.com/api/adventurer/".$email.".svg?skinColor=variant02",
 			"custom" => ["email" => $email, "lastOrder" => $orderID]
 			];
@@ -91,7 +91,7 @@ while($row = $resultpending->fetch_assoc()) {
 			curl_close($ch2);
 			echo $result2;
 			//END CREATE CONVERSATION
-*/
+
 
 
 
@@ -106,49 +106,101 @@ while($row = $resultpending->fetch_assoc()) {
 				$sqlupdate = "UPDATE `abandoned` SET `first`='1' WHERE id='$id'";
 				if ($conn->query($sqlupdate) === TRUE) {
 					echo "sending first message";
+					$message = "This is first message trying to recover your order";
+
+					//SEND MESSAGE TO TALKJS
+					$ch = curl_init();
+					$data = [
+					[
+						"sender"  => "administrator",
+						"text" => $message,
+						"type" => "UserMessage"
+					]];
+					$data1 = json_encode($data);
+					curl_setopt($ch, CURLOPT_URL, 'https://api.talkjs.com/v1/zQQphoB0/conversations/' . $orderID . '/messages');
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
+					$headers = array();
+					$headers[] = 'Content-Type: application/json';
+					$headers[] = 'Authorization: Bearer sk_live_SMK73rLbx7kUaOJ2Pur99ZE6RVnygEVv';
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					$result = curl_exec($ch);
+					if (curl_errno($ch)) {
+						echo 'Error:' . curl_error($ch);
+					}
+					curl_close($ch);
+
 				}
 
 			//Sending Second Message
-			}elseif($difference >= "40" && $first == "1" && $second == "0"){
+			}elseif($difference >= "1440" && $first == "1" && $second == "0"){
 
 				$sqlupdate = "UPDATE `abandoned` SET `second`='1' WHERE id='$id'";
 				if ($conn->query($sqlupdate) === TRUE) {
 					echo "sending second message";
+					$message = "This is second message trying to recover your order";
+
+					//SEND MESSAGE TO TALKJS
+					$ch = curl_init();
+					$data = [
+					[
+						"sender"  => "administrator",
+						"text" => $message,
+						"type" => "UserMessage"
+					]];
+					$data1 = json_encode($data);
+					curl_setopt($ch, CURLOPT_URL, 'https://api.talkjs.com/v1/zQQphoB0/conversations/' . $orderID . '/messages');
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
+					$headers = array();
+					$headers[] = 'Content-Type: application/json';
+					$headers[] = 'Authorization: Bearer sk_live_SMK73rLbx7kUaOJ2Pur99ZE6RVnygEVv';
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					$result = curl_exec($ch);
+					if (curl_errno($ch)) {
+						echo 'Error:' . curl_error($ch);
+					}
+					curl_close($ch);
+
+					
 				}
 			
 			//Sending Third Message
 			}elseif($difference >= "2880" && $first == "1" && $second == "1" && $third == "0"){
 				
-				$sqlupdate = "UPDATE `abandoned` SET `third`='1' WHERE id='$id'";
-				if ($conn->query($sqlupdate) === TRUE) {
 					echo "sending third message";
-				}
+					$message = "This is third message trying to recover your order";
+
+					//SEND MESSAGE TO TALKJS
+					$ch = curl_init();
+					$data = [
+					[
+						"sender"  => "administrator",
+						"text" => $message,
+						"type" => "UserMessage"
+					]];
+					$data1 = json_encode($data);
+					curl_setopt($ch, CURLOPT_URL, 'https://api.talkjs.com/v1/zQQphoB0/conversations/' . $orderID . '/messages');
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
+					$headers = array();
+					$headers[] = 'Content-Type: application/json';
+					$headers[] = 'Authorization: Bearer sk_live_SMK73rLbx7kUaOJ2Pur99ZE6RVnygEVv';
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					$result = curl_exec($ch);
+					if (curl_errno($ch)) {
+						echo 'Error:' . curl_error($ch);
+					}
+					curl_close($ch);
+
 
 			}	
-/*
-			//SEND MESSAGE TO TALKJS
-			$ch = curl_init();
-			$data = [
-			[
-				"sender"  => "administrator",
-				"text" => $message,
-				"type" => "UserMessage"
-			]];
-			$data1 = json_encode($data);
-			curl_setopt($ch, CURLOPT_URL, 'https://api.talkjs.com/v1/zQQphoB0/conversations/' . $orderID . '/messages');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data1);
-			$headers = array();
-			$headers[] = 'Content-Type: application/json';
-			$headers[] = 'Authorization: Bearer sk_live_SMK73rLbx7kUaOJ2Pur99ZE6RVnygEVv';
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			$result = curl_exec($ch);
-			if (curl_errno($ch)) {
-				echo 'Error:' . curl_error($ch);
-			}
-			curl_close($ch);
-*/
+
+			
+		}
 
  
             
