ca0a5980288b6dee3aaa297fc7e41055a465d4e0
IvanSimic95 <email@isimic.com>
2023-10-03 05:27:04 +0200

    Create husband.php

---
+++ offer/husband.php
@@ -0,0 +1,341 @@
+<?php
+$title = "Special One-Time-Offer!";
+$description = "Almost Complete...";
+include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/header.php'; 
+
+
+$t_product_name = "personal";
+$lower = strtolower($t_product_name);
+$sql = "SELECT * FROM review_total WHERE product = '".$t_product_name."'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$count = $row['reviews'];
+?>
+<script src="https://www.digistore24.com/service/digistore.js"></script><script>digistoreUpsell()</script>
+
+<style>
+     .footer-guarantee{
+        display:none;
+    }
+.btn-outline-reg {
+	background-color:#fff;
+	color: #ec5540;
+}
+
+.btn-outline-reg:hover {
+    background-color: #ec5540;
+    color: #fff;
+    text-decoration: none;
+}
+.ex-6-header {padding-bottom: 10px;}
+
+
+.ex-6-header h2 {
+    font-size: 30px;
+    line-height: 30px;
+    color: #333;
+    font-weight: 700;
+}
+
+
+.blob.orange {
+	background: rgba(255, 121, 63, 1);
+	box-shadow: 0 0 0 0 rgba(255, 121, 63, 1);
+	animation: pulse-orange 2s infinite;
+	
+	border-radius: 0.25rem;
+	height: 2rem;
+	font-weight: 600;
+	font-size: 17px;
+    padding: 8px 8px;
+	
+	max-width: 350px;
+    margin: auto;
+	
+}
+
+@keyframes pulse-orange {
+	0% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(255, 121, 63, 0.7);
+	}
+	
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px rgba(255, 121, 63, 0);
+	}
+	
+	100% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(255, 121, 63, 0);
+	}
+}
+
+
+.blob.purple {
+	background: rgba(142, 68, 173, 1);
+	box-shadow: 0 0 0 0 rgba(142, 68, 173, 1);
+	animation: pulse-purple 2s infinite;
+	text-decoration:none;
+	border-radius: 0.25rem;
+	height: 2rem;
+	font-weight: 600;
+	font-size: 18px;
+    padding: 10px 10px;	
+	
+	max-width: 350px;
+    margin: auto;
+	
+}
+
+@keyframes pulse-purple {
+	0% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(142, 68, 173, 0.7);
+	}
+	
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px rgba(142, 68, 173, 0);
+	}
+	
+	100% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(142, 68, 173, 0);
+	}
+}
+
+
+.blob.blue {
+	background: rgba(12, 109, 229, 1);
+	box-shadow: 0 0 0 0 rgba(12, 109, 229, 1);
+	animation: pulse-purple 2s infinite;
+	line-height: 16px;
+	border-radius: 0.25rem;
+	height: 4rem;
+	font-weight: 600;
+	font-size: 24px;
+    padding: 16px 8px;	
+	text-decoration:none;
+	max-width: 350px;
+    margin: auto;
+}
+
+@keyframes pulse-blue {
+	0% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(12, 109, 229, 0.7);
+	}
+	
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px rgba(12, 109, 229, 0);
+	}
+	
+	100% {
+		transform: scale(0.95);
+		box-shadow: 0 0 0 0 rgba(12, 109, 229, 0);
+	}
+}
+
+@property --progress-value {
+  syntax: '<integer>';
+  inherits: true;
+  initial-value: 0;
+}
+
+:root {
+  --progress-bar-color: #cfd8dc;
+  --progress-value-color: #2196f3;
+  --progress-empty-color-h: 4.1;
+  --progress-empty-color-s: 89.6;
+  --progress-empty-color-l: 58.4;
+  --progress-filled-color-h: 122.4;
+  --progress-filled-color-s: 39.4;
+  --progress-filled-color-l: 49.2;
+}
+
+progress[value] {
+  display: block;
+  position: relative;
+  appearance: none;
+  width: 100%;
+  height: 8px;
+  border: 0;
+  --border-radius: 10px;
+  border-radius: var(--border-radius);
+  counter-reset: progress var(--progress-value);
+  --progress-value-string: counter(progress) '%';
+  --progress-max-decimal: calc(var(--value, 0) / var(--max, 0));
+  --progress-value-decimal: calc(var(--progress-value, 0) / var(--max, 0));
+  @supports selector(::-moz-progress-bar) {
+    --progress-value-decimal: calc(var(--value, 0) / var(--max, 0));
+  }
+  --progress-value-percent: calc(var(--progress-value-decimal) * 100%);
+  --progress-value-color: hsl(
+    calc((var(--progress-empty-color-h) + (var(--progress-filled-color-h) - var(--progress-empty-color-h)) * var(--progress-value-decimal)) * 1deg)
+    calc((var(--progress-empty-color-s) + (var(--progress-filled-color-s) - var(--progress-empty-color-s)) * var(--progress-value-decimal)) * 1%)
+    calc((var(--progress-empty-color-l) + (var(--progress-filled-color-l) - var(--progress-empty-color-l)) * var(--progress-value-decimal)) * 1%)
+  );
+  animation: calc(3s * var(--progress-max-decimal)) linear 0.5s 1 normal both progress;
+}
+
+progress[value]::-webkit-progress-bar {
+  background-color: var(--progress-bar-color);
+  border-radius: var(--border-radius);
+  overflow: hidden;
+}
+
+progress[value]::-webkit-progress-value {
+  width: var(--progress-value-percent) !important;
+  background-color: var(--progress-value-color);
+  border-radius: var(--border-radius);
+}
+
+progress[value]::-moz-progress-bar {
+  width: var(--progress-value-percent) !important;
+  background-color: var(--progress-value-color);
+  border-radius: var(--border-radius);
+}
+
+progress[value]::after {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  --size: 35px;
+  width: var(--size);
+  height: 20px;
+  position: absolute;
+  left: var(--progress-value-percent);
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--progress-value-color);
+  border-radius: 10%;
+  content: attr(value);
+  content: var(--progress-value-string, var(--value));
+  font-size: 12px;
+  font-weight: 500;
+  color: #fff;
+}
+
+@keyframes progress {
+	from {
+		--progress-value: 0;
+	} to {
+		--progress-value: var(--value);
+	}
+}
+.navbar{
+    display:none;
+}
+.ex-6-header{
+padding-top:1rem;
+}
+
+.ex-6-header p {
+    max-width: none;
+}
+.img-fluid{
+    border-radius:1rem;
+}
+.contentinside{
+		border: 2px solid orange;
+    border-radius: 0.5rem;
+    background-color: white;
+	margin-right: 0px;
+	margin-left: 0px;
+	border-radius:0.5rem;
+	}
+    @media only screen and (max-width: 600px) {
+		.ex-6-header p {
+			max-width: 24rem;
+    margin-right: auto;
+    margin-left: auto;
+}
+}
+</style>
+<?php
+if(isset($_SESSION['shortproduct'])){
+    $drawingtext = $_SESSION['shortproduct'];
+    $text = 'Capturing the portrait of your <span style="font-weight:600;color:#8e44ad;">'.$drawingtext.'</span> in art is only the beginning';
+}else{
+    $text = 'Capturing the portrait in art is only the beginning';
+}
+
+?>
+
+ 
+    <!-- Header -->
+    <header class="ex-6-header">
+        <div class="header-content">
+            <div class="container">
+	
+	<div class="row contentinside">
+         <div class="col-lg-12 col-xl-12">
+            <div style="margin-top: 15px;margin-bottom:15px;" class="row">
+ 
+                    
+					
+                <div class="col-lg-12">
+					<div class="row m-0 p-0 progress-phone" style="border:1px solid;border-color:#7dc443;border-radius:0.5rem;">
+                        <div class="col-sm-4 col-4 m-auto" style="padding: 10px;font-weight:500;font-size: 12px;line-height: 15px;"><span style="font-weight:700;">STEP #1</span><br/>PAYMENT COMPLETE</div>
+                        <div class="col-sm-4 col-4 m-auto" style="padding: 10px;background-color: #7dc443; color:white; font-weight:700;font-size: 12px;line-height: 15px;"><span style="font-weight:700;">STEP #2</span><br/>CUSTOMIZE ORDER</div>
+                        <div class="col-sm-4 col-4 m-auto" style="padding: 10px;font-weight:500;font-size: 12px;line-height: 15px;"><span style="font-weight:700;">STEP #3</span><br/>ORDER COMPLETED</div>
+                    </div>
+					
+                    <progress value="50" max="100" style="--value: 50; --max: 100;margin-top:25px;margin-bottom:25px;"></progress>	
+                </div>
+				
+				<div class="col-lg-12 col-xl-12">
+				
+				<h2 style="margin-bottom: 24px;text-align:left;color:#8e44ad;" class="h2-heading">A GLIMPSE INTO YOUR DESTINED PARTNERSHIP</h2>
+		
+				<p style="margin-top: 5px;margin-bottom: 25px;text-align:left;color: #000;font-size: 18px;line-height: 1.4em;" class="card-title-prod">
+                <br> <p>Step into the realm of the unknown and unveil the face of your future life partner with the <strong>Future Spouse Psychic Drawing.</strong> While fate writes a captivating story, I offer you the chance to peek into its pages, introducing you to the one destined to stand by your side.</p>
+<br> <p>Every sketch I craft is a harmonious blend of intuitive insights, psychic vibrations, and artistry. Using a mix of clairvoyance and deep spiritual connection, I embark on a journey through time, capturing the essence of the person with whom you&apos;ll share life&apos;s most intimate moments.</p>
+<br> <p>To initiate this sacred process, I&apos;ll need your name and birthdate. Guided by the stars and the universe&apos;s whispers, I&apos;ll channel your future spouse&apos;s energy. The resulting portrait transcends mere lines and shades; it echoes with the laughter, dreams, and shared moments awaiting you.</p>
+<br> <p>Unveiling your future spouse isn&apos;t just about knowing their face. It&apos;s about preparing your heart for the love, lessons, and shared experiences that will define your journey together.</p>
+<br> <p><strong>EXCLUSIVE PERSPECTIVE: Deciphering the Astral Blueprint of Your Romantic Destiny</strong></p>
+<p>With each intricately crafted portrait, I journey deeper into the celestial tapestry, unveiling the intricate weave of fates that connect you to your future spouse. Discover the preordained encounters that will seamlessly merge your paths, the trials destined to solidify your union, and the pivotal moments that will shape the core of your shared experiences. By interpreting this astral blueprint, I illuminate the profound nuances, commitments, and ethereal pledges sculpted by the universe for your shared journey.</p>
+<br> <p>From individuals seeking clarity to celebrated personalities wanting a glance into their love future, my drawings have illuminated the path for many. Choosing the &quot;Future Spouse Psychic Drawing&quot; is more than a purchase; it&apos;s an invitation to a transformative experience of love and self-discovery.</p>
+<br> <p><strong>OUR VOW OF TRUST: A Bond That Respects Your Faith</strong></p>
+<p>If my vision doesn&apos;t unfold as perceived within the provided timeframe, a full refund will be honored, a testament to our shared trust and spiritual commitment.</p>
+<br> <p><strong>UTMOST CONFIDENTIALITY: Digital Delivery Ensuring Privacy</strong></p>
+<p>Your uniquely crafted drawing and accompanying insights will be discreetly delivered to your provided email, safeguarding your privacy. While the universe may know your story, our journey together remains confidential.</p>
+                    
+				</p>
+				   
+					    <div style="margin-bottom: 20px;background-color:#f8f1fd;" class="card-image">
+                        <img class="img-fluid" src="/images/products/personal.png" style="width:450px;display:none;">
+							<div style="padding:5px;margin-bottom:0px;">
+
+						<h2 style="font-size: 40px;background:none;color:#000;font-weight:800;margin-top:15px;line-height:30px;">$17<br/><span style="opacity: 0.25;font-weight:500;font-size:20px;">$46</span></h2> 
+						<p style="margin-top: -10px;">50% discount applied</p>
+
+				<div style="margin-bottom: -8px;margin-top: -17px;font-size: 12px;;">
+					<span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span>
+					<span style="font-size:11px;font-weight:600;"><?php echo $count; ?> reviews</span>
+				</div>
+				<br clear="all">
+								<a href="https://www.digistore24.com/answer/yes?template=light" style="color: #fff;" class="blob purple">🔮 Yes, I Want My Personal Psychic Reading</a>
+								<p></p>
+							<br>	<a href="https://www.digistore24.com/answer/no" style="font-size: 18px;color:blue;" href="">No Thanks, I'll remain in the shadows of uncertainty</a>
+								<p style="letter-spacing: -0.25px;font-size: 13px;line-height: 1.2;margin-top: 20px;">This special offer is only here and now. <span style="font-weight:600;">If you leave this page, you'll lose out on the chance to get the discount</span>. So, <span style="color:#8e44ad;font-weight:600;text-decoration:underline;">click the magic button now</span>!</p>
+								
+								<p style="letter-spacing: -0.25px;font-size: 12px;line-height: 1.2;margin-top: 20px;">
+									<span style="color: #09c100;font-weight:700;">$</span> Get a 60 day money back guarantee if you aren't satisfied.
+								</p>
+							</div>
+						</div>
+                </div>
+            </div>
+         </div>
+	</div>
+
+<br clear="all">			
+				
+            </div>  
+        </div>  
+    </header> 
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/templates/footer.php'; ?>
