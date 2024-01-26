e07d0bb0ce3cc4f5ed3809cff682f223571698ff
IvanSimic95 <email@isimic.com>
2024-01-26 21:58:00 +0100

    Update footer.php

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -102,6 +102,14 @@ text-align: left;margin-top: 15px;
 
 
     <?php if ($startpixel == 1) { ?>
+        <!-- Twitter conversion tracking base code -->
+<script>
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','ojcyv');
+</script>
+<!-- End Twitter conversion tracking base code -->
 <!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
@@ -134,14 +142,6 @@ fbq('init', '<?php echo $FBPixel; ?>');
 fbq('track', 'PageView');
 </script>
 
-<!-- Twitter conversion tracking base code -->
-<script>
-!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-twq('config','ojcyv');
-</script>
-<!-- End Twitter conversion tracking base code -->
 
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=<?php echo $FBPixel; ?>&ev=PageView&noscript=1"
