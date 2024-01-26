13b54f52ca2d16efe4f0f8e71ca5db42660a32c2
IvanSimic95 <email@isimic.com>
2024-01-26 22:01:12 +0100

    tw

--- offer/color.php
+++ offer/color.php
@@ -448,6 +448,9 @@ fbq('track', 'Purchase', {
 
 <!-- Twitter conversion tracking event code -->
 <script type="text/javascript">
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
   // Insert Twitter Event ID
   twq('event', 'tw-ojcyv-ojcyw', {
     value: $orderPrice, // use this to pass the value of the conversion (e.g. 5.00)
