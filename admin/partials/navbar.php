2a95726df3f78729c82e68a4a051ca43eabbedc1
IvanSimic95 <email@isimic.com>
2024-02-05 03:33:01 +0100

    Update navbar.php

--- admin/partials/navbar.php
+++ admin/partials/navbar.php
@@ -44,7 +44,7 @@
                                 Messages (TalkJS)
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse show" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="inbox.php"><div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div> All Messages</a>
                                     <a class="nav-link" href="inbox-unread.php"><div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div> Unread Messages</a>
