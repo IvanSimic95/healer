443a82c3ceeb0a9cb4aa1542d3a06d92f8604eaa
IvanSimic95 <email@isimic.com>
2024-02-05 03:14:43 +0100

    Update navbar.php

--- admin/partials/navbar.php
+++ admin/partials/navbar.php
@@ -52,22 +52,7 @@
                             </div>
 
                            
-                            <a class="nav-link" href="reviews.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-user-edit"></i></div>
-                                Reviews
-                            </a>
-                            <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="true" aria-controls="collapseLayouts2">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Blog
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="posts.php"><div class="sb-nav-link-icon"><i class="fas fa-list"></i></div> All posts</a>
-                                    <a class="nav-link" href="blog.php"><div class="sb-nav-link-icon"><i class="fas fa-plus"></i></div> Add New Post</a>
-                                </nav>
-                            </div>
-
+    
 
 
                             <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts3" aria-expanded="true" aria-controls="collapseLayouts2">
