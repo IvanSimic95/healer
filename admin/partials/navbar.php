3a81e2b53d32d983dcd1329909f410506576b014
IvanSimic95 <email@isimic.com>
2024-02-05 03:14:26 +0100

    Update navbar.php

--- admin/partials/navbar.php
+++ admin/partials/navbar.php
@@ -70,12 +70,12 @@
 
 
 
-                            <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="true" aria-controls="collapseLayouts2">
+                            <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts3" aria-expanded="true" aria-controls="collapseLayouts2">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Logs
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapseLayouts3" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="abandoned.php"><div class="sb-nav-link-icon"><i class="fas fa-list"></i></div> Abandoned Log</a>
                                 </nav>
